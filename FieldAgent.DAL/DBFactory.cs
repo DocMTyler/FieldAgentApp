d52dd3723c94d487b43b95504013b9c3f4c55cbf
Tyler Hougland <mtylerhouglandphd@gmail.com>
2022-04-22 11:38:07 -0500

    Add AppDbContext

---
+++ FieldAgent.DAL/DBFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace FieldAgent.DAL
+{
+    public class DBFactory
+    {
+        private readonly IConfigurationRoot Config;
+
+        public DBFactory(IConfigurationRoot config)
+        {
+            Config = config;
+        }
+
+        public AppDbContext GetDbContext()
+        {
+            return new AppDbContext();
+        }
+    }
+}
